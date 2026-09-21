e222067ff8a2e26fdae580b89a932fae9dc87dea
sngtau19-collab <sngtau19@gmail.com>
2026-09-21 19:29:53 +0900

    Add files via upload

---
+++ kasir-pro/sw.js
@@ -0,0 +1,5 @@
+const CACHE='KASIR_TOKO_PRO_V1';
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const c=x.clone();caches.open(CACHE).then(k=>k.put(e.request,c));return x}).catch(()=>caches.match('./index.html')))));
